ff0e38bce78d20353555b6e1e201ff2891d1bf3e
Malik Mohammad Mudasir <98544613+Mudasir4736@users.noreply.github.com>
2023-05-18 12:36:48 +0530

    first commit

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+           <img src="https://wpforms.com/wp-content/uploads/2021/02/tune-contact-us-page-examples.png" alt="" width='100%' height='100%' />
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <img src="https://assets.hongkiat.com/uploads/getting-started-react-js/react-home.jpg?newedit" alt=""  width='100%'/>
+        </div>
+    );
+}
+
+export default Home;
